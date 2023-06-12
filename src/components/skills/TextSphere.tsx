18857bb912ddf7b58a558d5540fb69d1a36f0ad2
Mohamed-Echbiy <echbiymed@gmail.com>
2023-06-12 22:55:05 +0100

    fix th sphere

--- src/components/skills/TextSphere.tsx
+++ src/components/skills/TextSphere.tsx
@@ -1,54 +1,46 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { useEffect, useRef } from "react";
-
-// Importing TagCloud package
-import TagCloud from "TagCloud";
-
+import { TagCloud } from "@frank-mayer/react-tag-cloud";
+import { Cursor } from "react-creative-cursor";
+const texts = [
+  "Html",
+  "CSS",
+  "SCSS",
+  "JavaScript",
+  "React",
+  "NextJs",
+  "Solid",
+  "NodeJS",
+  "Sanity CMS",
+  "Tailwindcss",
+  "ES6",
+  "GIT",
+  "GITHUB",
+  "REST API",
+  "Jest",
+  "DaisyUI",
+  "Material UI",
+  "MongoDb",
+  "Firebase",
+  "clerk Auth",
+];
 const TextShpere = () => {
   // Animation settings for Text Cloud
-  useEffect(() => {
-    return () => {
-      const container = ".tagCloud";
-      const texts = [
-        "Html",
-        "CSS",
-        "SCSS",
-        "JavaScript",
-        "React",
-        "NextJs",
-        "Solid",
-        "NodeJS",
-        "Sanity CMS",
-        "Tailwindcss",
-        "ES6",
-        "GIT",
-        "GITHUB",
-        "REST API",
-        "Jest",
-        "DaisyUI",
-        "Material UI",
-        "MongoDb",
-        "Firebase",
-        "clerk Auth",
-      ];
-
-      const options = {
-        radius: 600,
-        maxSpeed: "fast",
-        initSpeed: "normal",
-        keep: true,
-      };
-
-      TagCloud(container, texts, options);
-    };
-  }, []);
 
   return (
     <>
-      <div className="text-shpere">
-        {/* span tag className must be "tagcloud"  */}
-        <span className="tagcloud"></span>
+      <Cursor isGelly={true} />
+      <div data-cursor-color="#fff" className="text-shpere">
+        <TagCloud
+          options={(w: Window & typeof globalThis): TagCloudOptions => ({
+            radius: Math.min(700, w.innerWidth, w.innerHeight) / 2,
+            maxSpeed: "fast",
+          })}
+          onClick={(tag: string, ev: MouseEvent) => alert(tag)}
+          onClickOptions={{ passive: true }}
+        >
+          {texts}
+        </TagCloud>
       </div>
     </>
   );
