a6e976410e7354bf90fe5b901a1c5e5b6d52d10a
Mohamed-Echbiy <echbiymed@gmail.com>
2023-06-13 15:03:49 +0100

    add projects links and social media links

--- src/components/Projects/ProjectLayout.tsx
+++ src/components/Projects/ProjectLayout.tsx
@@ -1,4 +1,7 @@
+import { FaCode, FaEye } from "react-icons/fa";
 import ImageSwiper from "./ImageSwiper";
+import Animate from "../AnimateLetters/Animate";
+import { useState, useEffect } from "react";
 
 interface ProjectData {
   title: string;
@@ -9,12 +12,42 @@ interface ProjectData {
 }
 
 function ProjectLayout(props: ProjectData) {
-  console.log(props);
+  const [letterClass, setLetterClass] = useState("text-animate");
+  useEffect(() => {
+    const setTime = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+    return clearTimeout(setTime);
+  }, []);
   return (
     <div className="project-container">
+      <div className="links-container">
+        <a href={props.viewLink} target="_blank" rel="noopener noreferrer">
+          <FaEye size="30px" color="white" />
+        </a>
+        {props.githubLink ? (
+          <a href={props.githubLink} target="_blank" rel="noopener noreferrer">
+            <FaCode size="30px" color="white" />
+          </a>
+        ) : (
+          <></>
+        )}
+      </div>
       <div className="title-container">
-        <h1 className="title">{props.title}</h1>
-        <h1 className="title_2">{props.title}</h1>
+        <h1 className="title">
+          <Animate
+            class={letterClass}
+            index={props.title.length + 5}
+            strArray={props.title.split("")}
+          />
+        </h1>
+        <h1 className="title_2">
+          <Animate
+            class={letterClass}
+            index={props.title.length + 5}
+            strArray={props.title.split("")}
+          />
+        </h1>
       </div>
       <div className="image">
         <ImageSwiper images={props.image} />
